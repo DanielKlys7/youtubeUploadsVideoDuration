7c6799fafc9c08e86978dadae8205c8762157a53
Daniel Opara <danielklys7@gmail.com>
2020-06-07 18:40:07 +0200

    remove regenerator-runtime

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// AIzaSyBYewgi2GlM1aAVb-jlFc3qjtg3vLfKe8g
-import 'regenerator-runtime/runtime'
-
 const getChannelUploads = async (userId) => {
   const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${userId}&key=${process.env.API_KEY}`);
   const data = await response.json()
